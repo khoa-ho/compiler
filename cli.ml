a1267ad2ab67b87e6d96bdfeb86e292418d3c421
hokhoa <hokhoa@grinnell.edu>
2018-02-01 23:53:02 -0600

    It works!

--- cli.ml
+++ cli.ml
@@ -1,22 +1,12 @@
-(* exemple.ml *)
+let print_length = function
+  arg -> print_endline (string_of_int (String.length arg))
 
-let length_flag = ref false
-
-(* let choose_mode = 
-  if flag then (
-    fun arg -> print_endline (string_of_int (String.length arg))
-  ) else (
-    fun arg -> print_endline arg
-  ) *)
+let print_arg = function
+  arg -> print_endline arg
 
 let main () =
-  let speclist = [("-l", Arg.Set length_flag, "Enables length-printing mode")] in 
-  let usage_msg = "Use -help for the list of options" in
-  let flag = !length_flag in 
-  if flag then (
-    Arg.parse speclist (fun arg -> print_endline (string_of_int (String.length arg))) usage_msg
-  ) else (
-    Arg.parse speclist (fun arg -> print_endline arg) usage_msg
-  )
+  let speclist = [("-length", Arg.Rest print_length, "Prints the lengths of each of the arguments")] in 
+  let usage_msg = "Usage: my-project [flags] [args]\nAvailable flags:" in
+  Arg.parse speclist (fun arg -> print_endline arg) usage_msg
 
 let _ = main ()
