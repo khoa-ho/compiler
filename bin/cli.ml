da4330c5bffc8de745b444a0135d0da473382133
khoa-ho <hokhoa@grinnell.edu>
2018-02-04 00:32:02 -0600

    Test git hooks

--- bin/cli.ml
+++ bin/cli.ml
@@ -2,11 +2,11 @@ let print_length = function
   | arg -> print_endline (string_of_int (String.length arg))
 
 let print_arg = function
-  | arg -> print_endline arg 
+  | arg -> print_endline arg
 
 let main () =
   let speclist = [("-length", Arg.Rest print_length, "Prints the lengths of each of the arguments")] in 
   let usage_msg = "Usage: my-project [flags] [args]\nAvailable flags:" in
   Arg.parse speclist print_endline usage_msg
 
-let _ = main ()   
+let _ = main () 
