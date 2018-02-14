784357180b9069d821595f56f949e551772afedb
khoa-ho <hokhoa@grinnell.edu>
2018-02-14 00:31:00 -0600

    add some boolean expressions

--- src/lang.ml
+++ src/lang.ml
@@ -1,14 +1,24 @@
 type exp =
   | EInt      of int
+  | EBool     of bool
   | EAdd      of exp * exp
   | ESubtract of exp * exp
   | EMultiply of exp * exp
   | EDivide   of exp * exp
+  | ELeq      of exp * exp
 
-let rec interpret (e:exp) : int =
+let rec interpret_int (e:exp) : int =
   match e with
   | EInt n             -> n
-  | EAdd (e1, e2)      -> interpret e1 + interpret e2
-  | ESubtract (e1, e2) -> interpret e1 + interpret e2
-  | EMultiply (e1, e2) -> interpret e1 * interpret e2
-  | EDivide (e1, e2)   -> interpret e1 / interpret e2
+  | EAdd (e1, e2)      -> interpret_int e1 + interpret_int e2
+  | ESubtract (e1, e2) -> interpret_int e1 + interpret_int e2
+  | EMultiply (e1, e2) -> interpret_int e1 * interpret_int e2
+  | EDivide (e1, e2)   -> interpret_int e1 / interpret_int e2
+  | _                  -> failwith "Not a valid integer operation!"
+
+let rec interpret_bool (e:exp) : bool =
+  match e with
+  | EBool b       -> b
+  | ELeq (e1, e2) -> interpret_int e1 <= interpret_int e2
+  | _             -> failwith "Not a valid boolean operation!"
+
