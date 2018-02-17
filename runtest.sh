09bdb39ba90e8a7ea242429f6e176935be9f8e36
khoa-ho <hokhoa@grinnell.edu>
2018-02-16 22:40:23 -0600

    Added test suite

--- runtest.sh
+++ runtest.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
 echo "Normal cases:"
-for sourcepath in `ls test/*.src`;
-do
-  sourcefile=$(basename $sourcepath)
-  testname=${sourcefile%.*}
-  outfile=${sourcepath%.*}.out
+num_file=0
 
-  if [ -e $outfile ]
-  then
-    diff <(./compiler.native $sourcepath) $outfile || (>&2 echo "Failed test '${testname}'!"; exit 1)
+for source_path in `ls test/*.src`; do
+  source_file=$(basename $source_path)
+  test_name=${source_file%.*}
+  out_file=${source_path%.*}.out
+
+  if [ -e $out_file ]; then
+    if ! diff <(./compiler.native $source_path) $out_file; then
+      >&2 echo "Failed test '${test_name}'!"
+      exit 1
+    fi
   else
-      (>&2 echo "Can't find '${outfile}'"; exit 1)
+    >&2 echo "Can't find '${out_file
+}'"
+    exit 1
   fi
-  echo "Pass '${testname}' test!"
+  let num_file++
 done
+echo "${num_file} tests passed!"
+
+
+echo "Error cases:"
+num_file=0
+
+for source_path in `ls test/*.err`; do
+  source_file=$(basename $source_path)
+  test_name=${source_file%.*}
+
+  ./compiler.native $source_path &> /dev/null
+  if [ $? == 0 ]; then
+    >&2 echo "Failed test '${test_name}'!"
+    exit 1
+  fi
+  let num_file++
+done
+echo "${num_file} tests passed!"
