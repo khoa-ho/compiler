13ef153417a83ff84f0148d2ed952b9ee9b35ebd
khoa-ho <hokhoa@grinnell.edu>
2018-02-17 16:33:51 -0600

    added command line interface

--- src/compiler.ml
+++ src/compiler.ml
@@ -1,7 +1,27 @@
-let main () =
-  let filename = Sys.argv.(1) in
+let is_lexing = ref false
+let is_parsing = ref false
+let files = ref []
+
+let cli () =
+  let speclist = 
+    [("-lex", Arg.Set is_lexing, "Enables lexing mode");
+     ("-parse", Arg.Set is_parsing, "Enable parsing mode");] 
+  in 
+  let usage_msg = "Usage: ./compiler.native [flags] [args]\nAvailable flags:" in
+  Arg.parse speclist (fun filename -> files :=  filename :: !files) usage_msg
+
+let compile filename =
   let tokens   = Lexer.lex (Stream.of_channel (open_in filename)) in
-  let (e, _)   = Parser.parse tokens in
-  Lang.interpret e |> Lang.string_of_exp |> print_endline
+  if !is_lexing then
+    tokens |> Lexer.string_of_token_list |> print_endline
+  else
+    let (e, _)   = Parser.parse tokens in
+    if !is_parsing then
+      Lang.string_of_exp e |> print_endline
+    else
+      Lang.interpret e |> Lang.string_of_exp |> print_endline
+let main () =
+  let _ = cli () in
+  List.iter compile !files
 
 let _ = if !Sys.interactive then () else main ()
