e4234c301feed5053c80189061b907954abd204b
khoa-ho <hokhoa@grinnell.edu>
2018-02-23 21:52:14 -0600

    added testing for typechecking

--- runtest.sh
+++ runtest.sh
@@ -7,7 +7,9 @@ for source_path in `ls test/*.src`; do
   testname=${source_file%.*}
   outfile=${source_path%.*}.out
   lexfile=${source_path%.*}.lex.out
-  parsefile=${source_path%.*}.parse.out  
+  parsefile=${source_path%.*}.parse.out
+  stepfile=${source_path%.*}.step.out 
+  typefile=${source_path%.*}.type.out
 
   if [ -e $outfile ]; then
     if ! diff <(./compiler.native $source_path) $outfile; then
@@ -19,7 +21,7 @@ for source_path in `ls test/*.src`; do
     exit 1
   fi
 
-    if [ -e $lexfile ]; then
+  if [ -e $lexfile ]; then
     if ! diff <(./compiler.native -lex $source_path) $lexfile; then
       >&2 echo "Failed test '${testname}'!"
       exit 1
@@ -29,13 +31,33 @@ for source_path in `ls test/*.src`; do
     exit 1
   fi
 
-    if [ -e $parsefile ]; then
+  if [ -e $parsefile ]; then
     if ! diff <(./compiler.native -parse $source_path) $parsefile; then
       >&2 echo "Failed test '${testname}'!"
       exit 1
     fi
   else
-    >&2 echo "Can't find '${parse}'"
+    >&2 echo "Can't find '${parsefile}'"
+    exit 1
+  fi
+
+  if [ -e $stepfile ]; then
+    if ! diff <(./compiler.native -step $source_path) $stepfile; then
+      >&2 echo "Failed test '${testname}'!"
+      exit 1
+    fi
+  else
+    >&2 echo "Can't find '${stepfile}'"
+    exit 1
+  fi
+
+  if [ -e $typefile ]; then
+    if ! diff <(./compiler.native -type $source_path) $typefile; then
+      >&2 echo "Failed test '${testname}'!"
+      exit 1
+    fi
+  else
+    >&2 echo "Can't find '${typefile}'"
     exit 1
   fi
 done
